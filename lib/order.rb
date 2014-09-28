cbce09421a1fe1bab99830c06ba17a49a10f2bf6
mala23 <mail@mala23.me>
2014-09-28 19:39:42 +0100

    Order can calculate the grand_total

--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,21 @@
 class Order
 
-	attr_accessor :line_items
+	attr_accessor :line_items, :grand_total
 
 	def initialize
 		@line_items = []
+		@grand_total = 0
 	end
 
 	def add_line_item(line_item)
 		@line_items << line_item
+	end
+
+	def grand_total
+		@line_items.each do |item|
+			@grand_total += item.total
+		end
+		@grand_total
 	end
 
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -13,12 +13,17 @@ describe Order do
 
 	end
 
-	it 'should be able to accept a LineItem' do
+	it 'should be able to add line items to the list' do
 		order.add_line_item(:line_item)
 		expect(order.line_items.count).to eq(1)
 	end
 
 	it 'should be able to calculate a grand_total' do
+		line1 = double :line_item, :total => 3
+		line2 = double :line_item, :total => 7
+		order.add_line_item(line1)
+		order.add_line_item(line2)
+		expect(order.grand_total).to eq(10)
 	end
 
 end
