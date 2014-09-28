20584a520ac7b8ce66e41cbee3db3235c2588971
mala23 <mail@mala23.me>
2014-09-28 18:29:35 +0100

    Menu has an array of dishes and can add dishes

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,2 +1,17 @@
 class Menu
+
+	attr_accessor :dishes
+
+	def initialize
+		@dishes = []
+	end
+
+	def add_dish(dish)
+		@dishes << dish
+	end
+
+	def has_dishes?
+		@dishes.any? == true
+	end
+
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -2,4 +2,16 @@ require 'menu'
 
 describe Menu do
 
+	let(:menu) { Menu.new }
+	let(:dish) { double :dish}
+
+	it "should have an array of dishes" do
+		expect(menu.dishes).to eq([])
+	end
+
+	it "should be able to add new dishes" do
+		menu.add_dish(dish)
+		expect(menu).to have_dishes
+	end
+
 end
