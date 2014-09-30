3f40cc70c06b8b18c633d46053d04623f0790dc6
mala23 <mail@mala23.me>
2014-09-30 08:50:00 +0100

    Wrote tests for class Takeaway

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,15 +1,2 @@
-require 'takeaway'
-
-describe Takeaway do
-
-	let(:takeaway) { Takeaway.new }
-
-	it 'should be able to return a list of dishes'
-	end
-
-	it 'should be able to return the price of a dish by its name'
-
-	it 'should be able to add an item to the order'
-	end
-
+class Takeaway
 end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,16 @@
+require 'takeaway'
+
+describe Takeaway do
+
+	let(:takeaway) { Takeaway.new }
+
+	it 'should be able to return a list of dishes' do
+	end
+
+	it 'should be able to return the price of a dish by its name' do
+	end
+
+	it 'should be able to add an item to the order'do
+	end
+
+end
